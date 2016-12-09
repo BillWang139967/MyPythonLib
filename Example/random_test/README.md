7efd5278863b27c1077d822f6ce40dde2e097c80
BillWang139967 <wangbin139967@163.com>
2016-12-09 10:25:38 +0800

    add:random_test

---
+++ Example/random_test/README.md
@@ -0,0 +1,18 @@
+---
+layout: post
+title: 随机选择问题和回答者
+subtitle:
+date: 2016-12-09 10:22:47
+category:
+author: meetbill
+tags: 
+   - 小例子
+---
+
+随机选择问题和回答者
+
+## 使用
+
+```
+#python random_test.py
+```
---
+++ Example/random_test/random_test.py
@@ -0,0 +1,55 @@
+﻿#!/usr/bin/python
+#coding=utf-8
+"""
+# Author: meetbill
+# Created Time : 2016-12-08 22:38:08
+
+# File Name: random_test.py
+# Description: 随机选择一道题，并且随机选择一个人进行作答
+
+"""
+import linecache
+import random
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def test():
+    f = [ x.replace('\n','') for x in linecache.getlines('doc/test.txt')]
+    random_int = random.randint(0, len(f)-1)
+    return f[random_int]
+
+def user():
+    f = [ x.replace('\n','') for x in linecache.getlines('doc/user.txt')]
+    random_int = random.randint(0, len(f)-1)
+    return f[random_int]
+          
+print('嘿嘿，开始选择今天的幸运使者:' )
+print """
+////////////////////////////////////////////////////////////////////  
+//                            _ooOoo_                             //  
+//                           o8888888o                            //      
+//                           88" . "88                            //      
+//                           (| -_- |)                            //      
+//                           O\  =  /O                            //  
+//                        ____/`---'\____                         //                          
+//                      .'  \\|     |//  `.                       //  
+//                     /  \\|||  :  |||//  \                      //      
+//                    /  _||||| -:- |||||-  \                     //  
+//                    |   | \\\  -  /// |   |                     //  
+//                    | \_|  ''\---/''  |   |                     //          
+//                    \  .-\__  `-`  ___/-. /                     //          
+//                  ___`. .'  /--.--\  `. . ___                   //      
+//                ."" '<  `.___\_<|>_/___.'  >'"".                //  
+//              | | :  `- \`.;`\ _ /`;.`/ - ` : | |               //      
+//              \  \ `-.   \_ __\ /__ _/   .-` /  /               //  
+//        ========`-.____`-.___\_____/___.-`____.-'========       //      
+//                             `=---='                            //  
+//        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^      //  
+//                         寻找有缘人                             //  
+//////////////////////////////////////////////////////////////////// 
+
+"""
+print "题目:",test()
+print
+print "幸运者:",user()
