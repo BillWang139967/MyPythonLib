99e7d7b37a0327f49fc971098fa272a7e1de9be2
meetbill <meetbill@163.com>
2017-08-24 17:50:54 +0800

    update:update command_utils

--- My_lib/command_utils/README.md
+++ My_lib/command_utils/README.md
@@ -1,28 +1,41 @@
 ## command_utils
 
-### code
+<!-- vim-markdown-toc GFM -->
+* [command_class](#command_class)
+* [command](#command)
+
+<!-- vim-markdown-toc -->
+
+### command_class
+
+作用：方便命令行调用模块中类中的方法（推荐）,更新程序时仅需添加方法,非常方便
 
 ```
-def hello(str_info):
-    print "hello",str_info
+[root@meetbill ~]# python command_class.py
+usage: python command_class.py function param [options]
 
-if __name__ == '__main__':
-    import sys
-    import inspect
+optional arguments:
+  -h, --help  show this help message and exit
+  None
+  Python Library Documentation: class ceshi_class in module __main__
+  class ceshi_class
+   |  ceshi
+   |
+   |  Methods defined here:
+   |
+   |  output(self, str_info)
+   |      eg:python command_class.py output "hello world"
+   |
+   |  output2(self, str_info='happy')
+   |      eg:python command_class.py output2 "hello world"
 
-    if len(sys.argv) < 2:
-        print "Usage:"
-        for k, v in globals().items():
-            if inspect.isfunction(v) and k[0] != "_":
-                print sys.argv[0], k, str(v.func_code.co_varnames[:v.func_code.co_argcount])[1:-1].replace(",", "")
-        sys.exit(-1)
-    else:
-        func = eval(sys.argv[1])
-        args = sys.argv[2:]
-        func(*args)
 ```
 
-## usage
+直接输入 `python command_class.py output "hello world"`即可调用此模块 `ceshi_class` 类中的 output 方法
+
+### command
+
+作用：方便调用模块中的方法
 
 ```
 #python command.py
--- My_lib/command_utils/command_class.py
+++ My_lib/command_utils/command_class.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+#coding=utf8
+"""
+# Author: meetbill
+# Created Time : 2017-08-24 17:29:17
+
+# File Name: command_class.py
+# Description:
+
+"""
 from __future__ import print_function
-
-import sys
-import os
-import logging
-import ConfigParser
-from pprint import pprint
-from pydoc import render_doc
-import argparse
-
 
 class ceshi_class():
 
@@ -16,38 +16,44 @@ class ceshi_class():
 
     def output(self, str_info):
         '''eg:python command_class.py output "hello world"'''
-        return 'hello %s' % str_info
+        print('hello %s' % str_info)
     def output2(self, str_info="happy"):
         '''eg:python command_class.py output2 "hello world"'''
-        return 'hello %s' % str_info
+        print('hello %s' % str_info)
 
 if __name__ == '__main__':
+    import os
+    from pydoc import render_doc
+    import argparse
     # Handle command-line options
     ceshi= ceshi_class()
     parser=argparse.ArgumentParser(usage='\033[43;37mpython %(prog)s function param [options]\033[0m')
-    parser.add_argument("-c", "--config", dest="config", default=os.environ['HOME'] + '/.config',
-                      help="CONFIG file", metavar="CONFIG")
-    parser.add_argument("-s", "--section", dest="section", default='default',
-                      help="Section of config file to use file", metavar="SECTION")
-    #(options, args) = parser.parse_args()
+    #parser.add_argument("-c", "--config", dest="config", default=os.environ['HOME'] + '/.config',
+    #                  help="CONFIG file", metavar="CONFIG")
+    #parser.add_argument("-s", "--section", dest="section", default='default',
+    #                  help="Section of config file to use file", metavar="SECTION")
     options, unknown_args = parser.parse_known_args()
     options = vars(options)
-    print(options)
-    print(unknown_args)
+
+    # options 命令行参数
+    # print(options)
+
+    # unknown_args 是类中的方法以及方法所需的参数(第一个值为类中的方法)
+    # print(unknown_args)
+
+    # 如果没有调用类中的方法
     if not unknown_args:
-        #print parser.print_usage()
         print(parser.print_help())
         print(render_doc(ceshi_class))
         exit()
+
     func = unknown_args.pop(0)
-    
     try:
         cmd = getattr(ceshi, func)
     except:
         print('No such function: %s' % func)
         print(render_doc(ceshi_class))
         exit()
-
     try:
         kwargs = {}
         func_args = []
@@ -58,10 +64,9 @@ if __name__ == '__main__':
             else:
                 func_args.append(arg)
         func_args = tuple(func_args)
+
+        # 此步执行程序
         function_result = cmd(*func_args, **kwargs)
     except TypeError:
         print(render_doc(cmd))
         exit()
-    print('********* Results ********')
-    pprint(function_result)
-    #print(len(function_result))
