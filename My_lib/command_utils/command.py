b6028694cda080d65bfa50f8295e679d6f284876
meetbill <meetbill@163.com>
2018-08-08 15:56:58 +0800

    [Update] update command_utils

--- My_lib/command_utils/command.py
+++ My_lib/command_utils/command.py
@@ -6,6 +6,7 @@ def hello(str_info):
 
 if __name__ == '__main__':
     import sys, inspect
+    import time
     if len(sys.argv) < 2:
         print "Usage:"
         for k, v in sorted(globals().items(), key=lambda item: item[0]):
@@ -18,8 +19,24 @@ if __name__ == '__main__':
                     print sys.argv[0], k, str(v.func_code.co_varnames[:v.func_code.co_argcount])[1:-1].replace(",", "")
         sys.exit(-1)
     else:
-        func = eval(sys.argv[1])
+        try:
+            func = eval(sys.argv[1])
+        except NameError:
+            print "Usage:"
+            for k, v in sorted(globals().items(), key=lambda item: item[0]):
+                if inspect.isfunction(v) and k[0] != "_":
+                    args, __, __, defaults = inspect.getargspec(v)
+                    if defaults:
+                        print sys.argv[0], k, str(args[:-len(defaults)])[1:-1].replace(",", ""), \
+                              str(["%s=%s" % (a, b) for a, b in zip(args[-len(defaults):], defaults)])[1:-1].replace(",", "")
+                    else:
+                        print sys.argv[0], k, str(v.func_code.co_varnames[:v.func_code.co_argcount])[1:-1].replace(",", "")
+            sys.exit(-1)
         args = sys.argv[2:]
+        now_start = int(time.time())
+        timeArray = time.localtime(now_start)
+        timeStr = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+        print "\x1B[;36m[start time]\x1B[0m:%s" % timeStr
         try:
             r = func(*args)
         except Exception, e:
@@ -31,6 +48,14 @@ if __name__ == '__main__':
             r = -1
             import traceback
             traceback.print_exc()
+        now_end = int(time.time())
+        timeArray = time.localtime(now_end)
+        timeStr = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+        print "\x1B[;36m[end time]\x1B[0m :%s" % timeStr
+        time_consum = now_end - now_start
+        time_consum_minute = time_consum / 60
+        time_consum_second = time_consum % 60
+        print "\x1B[;36m[consum time]\x1B[0m %s m:%s s" % (time_consum_minute,time_consum_second)
         if isinstance(r, int):
             sys.exit(r)
 
