4a0d27d6731bf568a10b7648ec47bce922824135
meetbill <meetbill@163.com>
2020-05-17 10:13:24 +0800

    [Changed]:update command_simple.py

--- My_lib/command_utils/command_simple.py
+++ My_lib/command_utils/command_simple.py
@@ -1,3 +1,31 @@
+#!/usr/bin/python
+# coding=utf8
+
+
+def ping():
+    """
+    Args:
+    """
+    print "pong"
+
+
+def echo1(str_info):
+    """
+    Args:
+        str_info: string
+    """
+    print "hello %s" % (str_info)
+
+
+def echo2(str_info1, str_info2):
+    """
+    Args:
+        str_info1: string
+        str_info2: string
+    """
+    print "hello %s %s" % (str_info1, str_info2)
+
+
 def hello(str_info, test="world"):
     """
     Args:
@@ -8,31 +36,72 @@ def hello(str_info, test="world"):
 
 
 if __name__ == '__main__':
-    import sys, inspect
-    if len(sys.argv) < 2:
-        print "Usage:"
+    import sys
+    import inspect
+
+    def _usage(func_name=""):
+        """
+        output the module usage
+        """
+        print("Usage:")
+        print("-------------------------------------------------")
         for k, v in sorted(globals().items(), key=lambda item: item[0]):
-            if inspect.isfunction(v) and k[0] != "_":
-                args, __, __, defaults = inspect.getargspec(v)
-                if defaults:
-                    print sys.argv[0], k, str(args[:-len(defaults)])[1:-1].replace(",", ""), \
-                          str(["%s=%s" % (a, b) for a, b in zip(args[-len(defaults):], defaults)])[1:-1].replace(",", "")
-                else:
-                    print sys.argv[0], k, str(v.func_code.co_varnames[:v.func_code.co_argcount])[1:-1].replace(",", "")
+            if func_name and func_name != k:
+                continue
+
+            if not inspect.isfunction(v) or k[0] == "_":
+                continue
+
+            args, __, __, defaults = inspect.getargspec(v)
+            #
+            # have defaults:
+            #       def hello(str_info, test="world"):
+            #               |
+            #               V
+            #       return: (args=['str_info', 'test'], varargs=None, keywords=None, defaults=('world',)
+            # no defaults:
+            #       def echo2(str_info1, str_info2):
+            #               |
+            #               V
+            #       return: (args=['str_info1', 'str_info2'], varargs=None, keywords=None, defaults=None)
+            #
+            # str(['str_info1', 'str_info2'])[1:-1].replace(",", "") ===> 'str_info1' 'str_info2'
+            #
+            if defaults:
+                args_all = str(args[:-len(defaults)])[1:-1].replace(",", ""), \
+                    str(["%s=%s" % (a, b) for a, b in zip(args[-len(defaults):], defaults)])[1:-1].replace(",", "")
+            else:
+                args_all = str(v.func_code.co_varnames[:v.func_code.co_argcount])[1:-1].replace(",", "")
+
+            if not isinstance(args_all, tuple):
+                args_all = tuple(args_all.split(" "))
+
+            exe_info = "{file_name} {func_name} {args_all}".format(
+                file_name=sys.argv[0],
+                func_name=k,
+                args_all=" ".join(args_all))
+            print(exe_info)
+
+            # output func_doc
+            if func_name and v.func_doc:
+                print("\n".join(["\t" + line.strip() for line in v.func_doc.strip().split("\n")]))
+
+        print("-------------------------------------------------")
+
+    if len(sys.argv) < 2:
+        _usage()
         sys.exit(-1)
     else:
         func = eval(sys.argv[1])
         args = sys.argv[2:]
         try:
             r = func(*args)
-        except Exception, e:
-            print "Usage:"
-            print "\t", "python %s" % sys.argv[1], str(func.func_code.co_varnames[:func.func_code.co_argcount])[1:-1].replace(",", "")
-            if func.func_doc:
-                print "\n".join(["\t\t" + line.strip() for line in func.func_doc.strip().split("\n")])
-            print e
+        except Exception:
+            _usage(func_name=sys.argv[1])
+
             r = -1
             import traceback
             traceback.print_exc()
+
         if isinstance(r, int):
             sys.exit(r)
